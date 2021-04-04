3e61a301cf7ba8850b7e747f69aacc69d23e551d
Jason C Fleming <jfleming@Jasons-MacBook-Pro.local>
2021-04-04 13:46:15 -0500

    more tweaks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,50 @@
-// JavaScript Document
+ (function($){
+    //"use strict";
+    function init(){
+        $( 'nav a, a.scroll-to, .scroll-to a, .tosection, a[href="#top"], button.scroll-to' ).on( 'click', scrollTo );
+        w.on( 'resize', resize );
+        resize();
+		$( '.mobile-toggle' ).on( 'click', toggle_menu );
+    }
+	 function toggle_menu( e ){
+		 e.preventDefault();
+	 	if ( $( '.mobile-toggle' ).hasClass( 'open' ) ){
+			$( '.mobile-nav-container' ).removeClass( 'open' );
+			$( '.mobile-toggle' ).removeClass( 'open' );
+		} else {
+			$( '.mobile-nav-container' ).addClass( 'open' );
+			$( '.mobile-toggle' ).addClass( 'open' );
+		}
+	 }
+     function resize(){
+         console.log( 'resize' );
+		 if ( $( '.breakflag' ).is( ':visible' ) ){
+			 if ( $( '.mobile-nav' ).length ){
+				 $( '.primary-nav' ).detach().appendTo( '.primary-nav-container' ).removeClass( 'mobile-nav' );
+			 } else {
+				 return;
+			 }
+		 } else {
+			 if ( $( '.mobile-nav' ).length ){
+				 return;
+			 } else {
+				 $( '.primary-nav' ).detach().appendTo( '.mobile-nav-container' ).addClass( 'mobile-nav' );
+			 }
+		 }
+     }
+   function scrollTo( e ){
+        console.log( 'scroll-to' );
+        e.preventDefault();
+        var el      = $( e.currentTarget ),
+            target  = el.attr( 'href' ),
+            adjust  = undefined === el.data( 'adjust' ) ? ( $( '.breakflag' ).is( ':visible' ) ? 160 : 0 ) : parseInt( el.data( 'adjust' ) ),
+            offset  = target && $( target).length ? $(target).offset().top - adjust : 0,
+            container = $( 'html, body' );
+        container.animate( { scrollTop: offset }, 'slow', function(){
+            history.replaceState( null, null, ' ' );
+        } );      
+	   $( '.mobile-nav-container,.mobile-toggle' ).removeClass( 'open' );
+    }
+    var w = $( window );
+    $( document ).ready( init );
+})(jQuery);
